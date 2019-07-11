ad50f48210c249e0bbc4c612c62d56850ca84707
Kenichi Asai <asai@is.ocha.ac.jp>
2019-07-11 18:48:58 +0900

    - update eval.js to include TransformToInt module - change the header for universe programs, etc

--- demos/universe/typed.js
+++ demos/universe/typed.js
@@ -58,18 +58,14 @@ Typed.BOOT = (function() {
 Typed.workspace = null;
 
 Typed.defaultCode =
-  "let place_image image zahyou scene = ?\n" +
-  "let rectangle xFloat yFloat color = ?\n" +
-  "let make_color r g b = ?\n" +
-  "let empty_scene xFloat yFloat = ?\n" +
-  "let width = ?\n" +
-  "let height = ?\n" +
-  "let background = empty_scene width height\n" +
+  "let width = 100\n" +
+  "let height = 200\n" +
+  "let background = ?\n" +
   "type world_t = {\n" +
-  "  a : int;\n" +
+  "  a : int * int;\n" +
   "}\n" +
   "\n" +
-  "let initial_world = ?\n" +
+  "let initial_world = {a = (50, 50)}\n" +
   "\n";
 
 Typed.init = function() {
@@ -160,10 +156,11 @@ Typed.showCode = function() {
 }
 
 Typed.programTop =
-  "open UniverseJs\n" +
-  "open Color\n" +
-  "open Image\n" +
-  "open World\n" +
+  "open UniverseJs;;\n" +
+  "open Color;;\n" +
+  "open Image;;\n" +
+  "open World;;\n" +
+  "open TransformToInt;;\n" +
   "\n";
 
 Typed.runCode = function() {
@@ -180,7 +177,9 @@ Typed.runGame = function() {
   newCanvas.id = 'CanvasForUniverse';
   canvas.replaceWith(newCanvas); // remove old canvas
   try {
+    Blockly.PrintSemiSemi = true;
     var code = Blockly.TypedLang.workspaceToCode(Typed.workspace);
+    Blockly.PrintSemiSemi = false;
     if (code.indexOf('type world_t =') === -1) {
       console.warn('The type world_t is not defined.');
     } else if (code.indexOf('let initial_world =') === -1) {
@@ -196,10 +195,10 @@ Typed.runGame = function() {
         program += "           ~to_draw:draw\n";
       }
       if (code.indexOf('let width =') !== -1) {
-        program += "           ~width:(int_of_float width)\n";
+        program += "           ~width:width\n";
       }
       if (code.indexOf('let height =') !== -1) {
-        program += "           ~height:(int_of_float height)\n";
+        program += "           ~height:height\n";
       }
       if (code.indexOf('let on_mouse ') !== -1) {
         program += "           ~on_mouse:on_mouse\n";
@@ -219,145 +218,13 @@ Typed.runGame = function() {
       if (code.indexOf('let draw_last ') !== -1) {
         program += "           ~to_draw_last:draw_last\n";
       }
-      program += "           ~onload:false\n";
+      program += "           ~onload:false;;\n";
       console.log(program);
-      evaluator.runGame(program);
-/*
-  runGame(' \
-open UniverseJs \
-open Color \
-open Image \
-open World \
- \
-(* constants *) \
-let radius =  30.               (* initial radius of balls *) \
-let width  = 500.               (* window width *) \
-let height = 400.               (* window height *) \
- \
-(* ball_t : type of balls *) \
-type ball_t = { \
-  center : float * float; \
-  vector : float * float; \
-  radius : float; \
-  color : Color.t; \
-} \
- \
-(* make_random_ball : float -> Color.t -> ball_t *) \
-let make_random_ball radius color = { \
-  center = (Random.float width, Random.float height); \
-  vector = (Random.float 3. -. 1., Random.float 3. -. 1.); \
-  radius = radius; \
-  color = color; \
-} \
- \
-(* ball_image : ball_t -> Image.t *) \
-let ball_image ball = match ball with \
-  {radius = r; color = c} -> circle r c \
- \
-(* ball_top_left : ball_t -> float * float *) \
-let ball_top_left ball = match ball with \
-  {center = (x, y); radius = r} -> (x -. r, y -. r) \
- \
-(* type of world *) \
-type world_t = ball_t list      (* a list of balls *) \
- \
-(* initial value of the world *) \
-let initial_world : world_t = \
-  [make_random_ball radius red; \
-   make_random_ball radius red; \
-   make_random_ball radius red] \
- \
-(* background : Image.t *) \
-let background = \
-  place_image (rectangle 50. 25. (make_color 0xbb 0xbb 0xbb)) \
-              (width /. 2. -. 25., height /. 2. -. 12.) \
-              (empty_scene width height) \
- \
-(* draw : world_t -> Image.t *) \
-let draw lob = \
-  place_images (List.map ball_image lob) \
-               (List.map ball_top_left lob) \
-               background \
- \
-(* draw_game_over : world_t -> Image.t *) \
-let draw_game_over world = \
-  place_image (text "Game Finished" ~size:30 black) \
-              (width /. 2., height /. 2.) \
-              (draw world) \
- \
-(* handle mouse event *) \
- \
-(* check if (x, y) is within r from (x0, y0) *) \
-(* is_inside : float -> float -> float * float -> float -> bool *) \
-let is_inside x y (x0, y0) r = \
-  (x -. x0) *. (x -. x0) +. (y -. y0) *. (y -. y0) <= r *. r  \
- \
-(* change_ball : ball_t -> ball_t *) \
-let change_ball {center = p; vector = v; radius = r; color = c} = \
-  if r <= 5. \
-  then make_random_ball radius c \
-  else make_random_ball (r -. 5.) c \
- \
-(* change_world_on_mouse : world_t -> float -> float -> string -> \
-                           (world_t, \'a) World.t *) \
-let change_world_on_mouse world x y event = match event with \
-    "button_down" -> \
-      let is_inside_ball ball = is_inside x y ball.center ball.radius in \
-      let change ball = if is_inside_ball ball \
-                        then change_ball ball \
-                        else ball in \
-      List.map change world \
-  | _ -> world \
- \
-(* handle tick event *) \
- \
-(* mod, but the result is always between 0 and b *) \
-(* mymod : float -> float -> float *) \
-let rec mymod a b = \
-  if a < 0. then mymod (a +. b) b \
-  else if b <= a then mymod (a -. b) b \
-  else a \
- \
-(* add_posn : float * float -> float * float -> float * float *) \
-let add_posn posn vector = match (posn, vector) with \
-  ((x1, x2), (v1, v2)) -> \
-    (mymod (x1 +. v1) width, mymod (x2 +. v2) height) \
- \
-(* move_ball_on_tick : ball_t -> ball_t *) \
-let move_ball_on_tick {center = p; vector = v; radius = r; color = c} = \
-  {center = add_posn p v; vector = v; radius = r; color = c}  \
- \
-(* change_world_on_tick : world_t -> (world_t, \'a) World.t *) \
-let change_world_on_tick world = \
-  List.map move_ball_on_tick world \
- \
-(* andmap : (\'a -> bool) -> \'a list -> bool *) \
-let rec andmap f lst = match lst with \
-    [] -> true \
-  | first :: rest -> f first && andmap f rest \
- \
-(* game_finished : world_t -> bool *) \
-let game_finished lob = \
-  andmap (fun ball -> ball.radius <= 5.) lob \
- \
-(* game start *) \
-let () = \
-  big_bang initial_world \
-           ~name:"Ball Game" \
-           ~to_draw:draw \
-           ~width:(int_of_float width) \
-           ~height:(int_of_float height) \
-           ~on_mouse:change_world_on_mouse \
-           (* ~on_key_press:change_world_on_key *) \
-           ~on_tick:change_world_on_tick \
-           ~rate:0.1 \
-           ~stop_when:game_finished \
-           ~to_draw_last:draw_game_over \
-	   ~onload:false \
-  '); */
+      evaluator.runCode(program);
     }
   } catch (e) {
-    console.warn('Some of blocks are not supported for converting.');
+    console.warn('Some of blocks are not supported for converting:');
+    console.warn(e);
   }
 }
 
