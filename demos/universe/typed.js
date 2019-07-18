303eb8b3d99d678ef1addc922a204e2515ab51aa
juko <sakikaorucha027@gmail.com>
2019-07-18 10:31:47 +0900

    Changed file

--- demos/universe/typed.js
+++ demos/universe/typed.js
@@ -304,7 +304,7 @@ Typed.clearToplevel = function() {
 Typed.onClickConvert = function(event) {
   event.preventDefault();
   var input = document.querySelector(".ocamlCode");
-  var code = codeEditor.getValue();
+  var code = Typed.codeEditor.getValue();
   if (code) {
     BlockOfOCamlUtils.codeToBlock(code);
   }
